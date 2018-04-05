82ad584ad5f08c1c6e02e022525d475ed8244d22
John Chleborad <jchleborad@gmail.com>
2018-04-05 08:00:09 -0700

    fist commit

---
+++ ngApp/controllers/controllers.js
@@ -0,0 +1,41 @@
+export class HomeController {
+  constructor() {
+  }
+}
+
+export class AboutController {
+  constructor() {
+    this.message = 'about page';
+  }
+}
+
+export class ResumeController {
+  constructor() {
+    this.message = 'resume page';
+  }
+}
+
+export class ActivitiesController {
+  constructor() {
+    this.message = 'activities page';
+  }
+}
+
+export class BlogController {
+  constructor() {
+    this.message = 'blog page';
+  }
+}
+export class PageazController {
+  constructor() {
+    this.message = 'Page Blog Post';
+  }
+}
+
+export class TodoController {
+  constructor() {
+    this.message = 'Todo List';
+  }
+}
+
+
